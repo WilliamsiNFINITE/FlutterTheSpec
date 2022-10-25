1bc12fd03a05bde18c6e6fba364aec40f3be79d1
WilliamsiNFINITE <williams.hoarau@ensta-bretagne.org>
2022-10-25 15:57:59 +0200

    refactor flutter_the_spec to flutterTheSpec to fix error in kotlin static analysis (2)

--- android/app/src/main/kotlin/com/example/flutterTheSpec/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutterTheSpec/MainActivity.kt
@@ -2,5 +2,6 @@ package com.example.flutter_the_spec
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    // This empty class is necessary to make the FlutterActivity
 }
