d5bdfcf41d8c65375447a48f1fd8a70a770812ec
WilliamsiNFINITE <williams.hoarau@ensta-bretagne.org>
2023-02-03 09:58:58 +0100

    add graphSceneState_test.dart

---
+++ test/graphSceneState_test.dart
@@ -0,0 +1,16 @@
+// This will test the methods in graphSceneState.
+
+
+import 'package:flutter_the_spec/widget/graph/graphSceneState.dart';
+
+void main() {
+    group('First Tests', (){
+        test('1 should be equal to 1'){
+            expect(1,1);
+        }
+        test('2 should not be equal to 1'){
+            expect(2!=1, true);
+        }
+    }
+}
+
